4973a4bda471a57b408a32c445f5236d43299684
Damian Pokorski <damianpokorski@hotmail.com>
2020-06-07 14:23:48 +0100

    Wrapping MVP

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,29 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const expressValidatorRejectInvalidRequests = require('./middleware/expressValidatorRejectInvalidRequests');
-
+const DataAcquisitionWorker = require('./classes/workers/DataAcquisitionWorker');
 /** @type {Router} */
 const routes = new Router();
+const providers = {
+  gas: process.env.PROVIDER_GAS_URL|| 'http://localhost:3000/api/gas',
+  internet: process.env.PROVIDER_INTERNET_URL || 'http://localhost:3000/api/internet',
+}
 
 
 routes.post('/api', [
   check('provider')
     .exists('provider').withMessage('Property [provider] must be defined').bail()
-    .isIn(['gas', 'internet']).withMessage(`Property must be gas or internet`),
+    .isIn(Object.keys(providers)).withMessage(`Property must be in ${Object.keys(providers).join(', ')}`),
   check('callbackUrl')
     .exists().withMessage('Property [callbackUrl] must be defined').bail(),
   expressValidatorRejectInvalidRequests
 ], (request, response) => {
-  return response.send('success');
+  (new DataAcquisitionWorker)
+    .addWork({
+      url: providers[request.body.provider],
+      callbackUrl: request.body.callbackUrl
+    })
+    .then(() => response.send('success'));
 });
 
 module.exports = routes;
